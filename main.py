98c02c6272f57ce5847e69359a43e54d43380d6d
LucasSchweisthal2024 <818482@mystma.org>
2021-09-28 15:04:21 -0500

    sdflkjsdfhsdfj

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 #WRITE YOUR CODE IN THIS FILE
+def close10(x, y):
+    10 - x
+    10 - y
+    if x > y:
+        return x
+    elif y < x:
+        return y
+    else:
+        return "they are equal"
+
+print(close10(9, 8))
